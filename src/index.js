c89a28e39f4e77aa23c63501b83ceaf3e1897521
Antkeo1 <sleav143@gmail.com>
2019-03-15 16:39:53 -0400

    created the logic and added some css

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+
+class App extends React.Component {
+  // initializing this.state
+  state = { lat: null, errorMessage: ''}
+
+  // lifecycle methods
+  componentDidMount () {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
+    )
+  }
+
+  componentDidUpdate () {
+    console.log('it updated')
+  }
+
+  //have to define render method or give it logic
+  // and conditions
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+    return <div>Loading!</div>
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
