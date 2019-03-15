8d2cf5163ff90ec328151d471dee54b0b5d9c48b
Antkeo1 <sleav143@gmail.com>
2019-03-15 17:25:56 -0400

    created a default message for spinner.js using defaultProps

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
-const Spinner = () => {
+const Spinner = (props) => {
   return (
     <div className="ui active dimmer">
       <div className="ui big text loader">
-        Loading...
+        {props.message}
       </div>
     </div>
   )
+}
+
+// if we dont put in props.message,
+// this will be default message
+Spinner.defaultProps = {
+  message: 'Loading...'
 }
 
 export default Spinner
