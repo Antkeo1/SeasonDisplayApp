8f31f7d547e0661edda157057dfb57418c8432c0
Antkeo1 <sleav143@gmail.com>
2019-03-16 15:13:07 -0400

    Updates

---
+++ precache-manifest.d6e474a175cef54be92573ae75f0d706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd834ddc2264594733e7",
+    "url": "/SeasonDisplayApp/static/js/runtime~main.dd834ddc.js"
+  },
+  {
+    "revision": "7f00e69703db3b4dfd1d",
+    "url": "/SeasonDisplayApp/static/js/main.7f00e697.chunk.js"
+  },
+  {
+    "revision": "49e8ed93f0c971c12a69",
+    "url": "/SeasonDisplayApp/static/js/2.49e8ed93.chunk.js"
+  },
+  {
+    "revision": "7f00e69703db3b4dfd1d",
+    "url": "/SeasonDisplayApp/static/css/main.19edd7bd.chunk.css"
+  },
+  {
+    "revision": "6646c9f7b452a883e4d74ba38b383015",
+    "url": "/SeasonDisplayApp/index.html"
+  }
+];
