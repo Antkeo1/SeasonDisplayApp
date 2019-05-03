fd002d05f6e813c3cd2582b0a1564ddc87207c66
Antkeo1 <sleav143@gmail.com>
2019-05-03 18:10:36 -0400

    turned App to a functional component using hooks

--- src/index.js
+++ src/index.js
@@ -2,44 +2,56 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 import Spinner from './Spinner'
+import useLocation from './useLocation'
 
-class App extends React.Component {
+const App = () => {
+  const [lat, errorMessage] = useLocation()
+
+  let content
+  if(errorMessage) {
+    content = <div>{errorMessage}</div>
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat}/>
+  } else {
+    content = <Spinner message="Please accept location request"/>
+  }
+  return (
+    <div className='border red'>
+      {content}
+    </div>
+  )
+}
+
+// class App extends React.Component {
   // initializing this.state
-  state = { lat: null, errorMessage: ''}
+  // state = { lat: null, errorMessage: ''}
 
   // lifecycle method
-  componentDidMount () {
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({lat: position.coords.latitude}),
-      err => this.setState({errorMessage: err.message})
-    )
-  }
+  // componentDidMount () {
 
-  componentDidUpdate () {
-    console.log('it updated')
-  }
+  // }
 
   // function with logic on rendering content = helper method
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div> Error: {this.state.errorMessage}</div>
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>
-    }
-    return <Spinner message="Please accept location request"/>
-  }
+  // renderContent() {
+    // if (this.state.errorMessage && !this.state.lat) {
+      // return <div> Error: {this.state.errorMessage}</div>
+    // }
+    // if (!this.state.errorMessage && this.state.lat) {
+      // return <SeasonDisplay lat={this.state.lat}/>
+    // }
+    // return <Spinner message="Please accept location request"/>
+  // }
 
   //have to define render method or give it logic
   // and conditions
-  render() {
-    return (
-      <div className='border red'>
-        {this.renderContent()}
-      </div>
-    )
-  }
-}
+  // render() {
+    // return (
+      // <div className='border red'>
+        // {this.renderContent()}
+      // </div>
+    // )
+  // }
+// }
 
 ReactDOM.render(
   <App />,
